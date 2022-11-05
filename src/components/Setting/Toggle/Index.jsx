18aa1ed057736cc2720fe316e0e96a4cb1219ce3
youbinpark <you8inpark@gmail.com>
2022-11-05 23:28:21 +0900

    Add: setting 페이지 토글버튼 기능 추가

--- src/components/Setting/Toggle/Index.jsx
+++ src/components/Setting/Toggle/Index.jsx
@@ -6,44 +6,42 @@ const Toggle = () => {
   const [toggle, setToggle] = useState(false);
 
   const onToggle = () => {
-    if (!toggle) {
-      setToggle(true);
-      return;
-    }
-    setToggle(false);
+    setToggle(!toggle);
   };
 
   return (
-    <>
-      <ToggleButton>
-        <div className='circle' onClick={onToggle} />
-      </ToggleButton>
-    </>
+    <ToggleBox>
+      <div className={toggle ? 'toggle-on' : 'toggle-off'} onClick={onToggle}>
+        <div className='circle' />
+      </div>
+    </ToggleBox>
   );
 };
 
-const ToggleButton = styled.div`
-  cursor: pointer;
-  display: flex;
-  -webkit-box-align: center;
-  align-items: center;
-  width: 2.875rem;
-  height: 1.5rem;
-  padding: 0.125rem;
+const ToggleBox = styled.div`
+  .toggle-on {
+    cursor: pointer;
+    display: flex;
+    -webkit-box-align: center;
+    align-items: center;
+    width: 2.875rem;
+    height: 1.5rem;
+    padding: 0.125rem;
 
-  border-radius: 1.125rem;
+    border-radius: 1.125rem;
 
-  background: ${primary2};
-  transition: all 0.125s ease-in 0s;
+    background: ${primary2};
+    transition: all 0.125s ease-in 0s;
 
-  .circle {
-    width: 1.25rem;
-    height: 1.25rem;
-    transform: translate(1.375rem);
+    .circle {
+      width: 1.25rem;
+      height: 1.25rem;
+      transform: translate(1.375rem);
 
-    border-radius: 0.625rem;
-    background: ${backgroundElement1};
-    box-shadow: rgb(0 0 0 / 10%) -2px 0px 4px;
+      border-radius: 0.625rem;
+      background: ${backgroundElement1};
+      box-shadow: rgb(0 0 0 / 10%) -2px 0px 4px;
+    }
   }
 
   .toggle-off {
