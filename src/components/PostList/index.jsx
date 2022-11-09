d949bb62be7d34d66928891a13b5ae0a2356a74e
sh.inv <104422865+sh-inv@users.noreply.github.com>
2022-11-09 21:20:41 +0900

    Feature/postlist infinite scroll (#10)
    * Add: 무한스크롤 구현중
    
    * Refactor: 데이터 갯수 조정 및 불필요한 코드 삭제

--- src/components/PostList/index.jsx
+++ src/components/PostList/index.jsx
@@ -1,20 +1,69 @@
+import { useRef, useState, useEffect } from 'react';
+import axios from 'axios';
+import styled from 'styled-components';
 import Post from './Post';
 import PostListNavBar from '../PostListNavBar';
-import styled from 'styled-components';
 import { maxWidth1056px, maxWidth1440px, maxWidth1920px, minWidth250px } from '../../styles/media';
 
 const PostList = () => {
-  const arr = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+  const [postData, setPostData] = useState([]);
+  const observerRef = useRef(null);
+  const [bottom, setBottom] = useState(null);
+  const [pageNum, setPageNum] = useState(1);
+
+  const getPostData = () => {
+    try {
+      (async () => {
+        const {
+          data: { data },
+        } = await axios.get('data/postlist/data.json');
+        setPostData([...postData, ...data]);
+      })();
+    } catch (error) {
+      console.log('error => ', error);
+    }
+  };
+
+  useEffect(getPostData, [pageNum]);
+
+  const intersectionObserver = entries => {
+    if (entries[0].isIntersecting) {
+      setPageNum(pageNum => pageNum + 1);
+    }
+  };
+
+  const observerOptions = {
+    threshold: 1,
+    rootMargin: '50px',
+  };
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(intersectionObserver, observerOptions);
+    observerRef.current = observer;
+  }, [postData]);
+
+  useEffect(() => {
+    const observer = observerRef.current;
+    if (bottom) {
+      observer.observe(bottom);
+      console.log('관찰시작');
+    }
+    return () => {
+      if (bottom) observer.unobserve(bottom);
+      console.log('관찰종료');
+    };
+  }, [bottom]);
 
   return (
     <PostListContainer>
       <PostListNavBar />
       <div className='post-list-out-box'>
         <div className='post-list-inner-box'>
-          {arr.map((el, i) => {
+          {postData.map((_, i) => {
             return <Post key={i} />;
           })}
         </div>
+        {postData.length && <div ref={setBottom} />}
       </div>
     </PostListContainer>
   );
