a3735c9dc27c5f3c42b16a4b8d08f56fca8bcfa2
sh.inv <qierapu1im@gmail.com>
2022-11-13 15:11:43 +0900

    Add: 글쓴시간 (분, 시간, 일, 주, 달, 년)전 단위로 보이는 함수 추가

--- src/components/PostList/Post/PostInfo/index.jsx
+++ src/components/PostList/Post/PostInfo/index.jsx
@@ -3,6 +3,25 @@ import styled from 'styled-components';
 import { text2 } from '../../../../styles/color';
 
 const PostInfo = () => {
+  const detailDate = date => {
+    const milliSeconds = new Date() - date;
+    const seconds = milliSeconds / 1000;
+    if (seconds < 300) return `방금 전`;
+    const minutes = seconds / 60;
+    if (5 < minutes < 60) return `${Math.floor(minutes)}분 전`;
+    const hours = minutes / 60;
+    if (hours < 24) return `${Math.floor(hours)}시간 전`;
+    const days = hours / 24;
+    if (days < 7) return `${Math.floor(days)}일 전`;
+    const weeks = days / 7;
+    if (weeks < 5) return `${Math.floor(weeks)}주 전`;
+    const months = days / 30;
+    if (months < 12) return `${Math.floor(months)}개월 전`;
+    const years = days / 365;
+    return `${Math.floor(years)}년 전`;
+  };
+  const nowDate = detailDate('글쓴시간');
+
   return (
     <PostInfoContainer>
       <Link to=''>
