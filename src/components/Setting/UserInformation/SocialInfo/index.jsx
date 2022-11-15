0e751fb8d65f0b156447ed16150f54183ad38954
Eden Park <109200582+daydreamplace@users.noreply.github.com>
2022-11-16 02:55:55 +0900

    Refactor: 소셜 정보 수정 코드 리팩토링 (#27)
    * Refactor: 소셜 정보 수정 코드 리팩토링
    
    * Remove: 안쓰는 state 함수 삭제
    
    * Refactor: 소셜 정보 수정 코드 리팩토링

--- src/components/Setting/UserInformation/SocialInfo/index.jsx
+++ src/components/Setting/UserInformation/SocialInfo/index.jsx
@@ -7,51 +7,67 @@ import styled from 'styled-components';
 import { backgroundElement1, border1, border3, text2, primary1, primary2, buttonText } from '../../../../styles/color';
 
 const SocialInfo = () => {
-  const [social, setSocial] = useState('');
   const [isModifySocialInfo, setisModifySocialInfo] = useState(false);
+  const [socialInfo, setSocialInfo] = useState({
+    email: '',
+    github: '',
+    twitter: '',
+    facebook: '',
+    url: '',
+  });
 
   const onModify = () => {
     isModifySocialInfo ? setisModifySocialInfo(false) : setisModifySocialInfo(true);
   };
 
-  const getSocial = e => {
-    setSocial(e.target.value);
+  useEffect(() => {
+    const contents = {
+      social: { email: 'you8inpark@gmail.com', github: 'daydreamplace', twitter: 'eden', facebook: 'eden', url: 'dev-eden.shop' },
+    };
+    setSocialInfo({
+      ...contents.social,
+    });
+  }, []);
+
+  const getSocialInfo = e => {
+    setSocialInfo({
+      ...socialInfo,
+      [e.target.name]: e.target.value,
+    });
   };
 
   const info = [
     {
       id: 1,
+      name: 'email',
       icon: <MdEmail className='icon' />,
       placeholder: '이메일을 입력하세요.',
     },
     {
       id: 2,
+      name: 'github',
       icon: <AiFillGithub className='icon' />,
       placeholder: 'Github 계정을 입력하세요.',
     },
     {
       id: 3,
+      name: 'twitter',
       icon: <AiOutlineTwitter className='icon' />,
       placeholder: 'Twitter 계정을 입력하세요.',
     },
     {
       id: 4,
+      name: 'facebook',
       icon: <AiFillFacebook className='icon' />,
       placeholder: 'http://www.facebook.com/',
     },
     {
       id: 5,
+      name: 'url',
       icon: <AiFillHome className='icon' />,
       placeholder: '홈페이지 주소를 입력하세요.',
     },
   ];
-
-  useEffect(() => {
-    const contents = {
-      social: { email: 'you8inpark@gmail.com', github: 'daydreamplace', twitter: 'eden', facebook: 'eden', url: 'dev-eden.shop' },
-    };
-    setSocial(contents.social);
-  }, []);
 
   return (
     <>
@@ -62,7 +78,7 @@ const SocialInfo = () => {
               {info.map(content => (
                 <li key={content.id}>
                   {content.icon}
-                  <input className='modify-input' type='text' placeholder={content.placeholder} />
+                  <input className='modify-input' type='text' name={content.name} placeholder={content.placeholder} value={socialInfo[content.name]} onChange={getSocialInfo} />
                 </li>
               ))}
             </ul>
@@ -77,23 +93,23 @@ const SocialInfo = () => {
             <ul className='save-info'>
               <li>
                 <MdEmail className='icon' />
-                <span>{social.email}</span>
+                <span>{socialInfo.email}</span>
               </li>
               <li>
                 <AiFillGithub className='icon' />
-                <span>{social.github}</span>
+                <span>{socialInfo.github}</span>
               </li>
               <li>
                 <AiOutlineTwitter className='icon' />
-                <span>{social.twitter}</span>
+                <span>{socialInfo.twitter}</span>
               </li>
               <li>
                 <AiFillFacebook className='icon' />
-                <span>{social.facebook}</span>
+                <span>{socialInfo.facebook}</span>
               </li>
               <li>
                 <AiFillHome className='icon' />
-                <span>{social.url}</span>
+                <span>{socialInfo.url}</span>
               </li>
             </ul>
           </SocialInfoContainer>
