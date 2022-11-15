790804668e7dbdc067ceccbeb687754927ef605d
Eden Park <109200582+daydreamplace@users.noreply.github.com>
2022-11-16 04:21:34 +0900

    Add: 내 블로그 유저 박스 레이아웃 구현 및 라우터 설정 (#28)
    * Add: 내 블로그 라우터 지정
    
    * Add: 내 블로그 폴더 추가
    
    * Add: 내 블로그 유저 박스 레이아웃 구현 및 라우터 설정
    
    * Modify: 유저 박스 이미지 마진 수정

---
+++ src/components/UserBox/User/index.jsx
@@ -0,0 +1,73 @@
+import { Link } from 'react-router-dom';
+import UserProfileImage from '../../Setting/UserProfile/UserProfileImage';
+import styled from 'styled-components';
+import { backgroundElement3, text1, text2 } from '../../../styles/color';
+
+const User = () => {
+  return (
+    <>
+      <UserContainer>
+        <Link to='/id'>
+          <UserProfileImage />
+        </Link>
+        <div className='user-info'>
+          <div className='user-name'>
+            <Link to='/id'>Eeeee</Link>
+          </div>
+          <div className='description'>ben1mki가나다</div>
+        </div>
+      </UserContainer>
+      <Border />
+    </>
+  );
+};
+
+const UserContainer = styled.div`
+  display: flex;
+  -webkit-box-align: center;
+  align-items: center;
+
+  img {
+    margin-bottom: 0;
+    box-shadow: rgb(0 0 0 / 6%) 0px 0px 4px 0px;
+  }
+
+  .user-info {
+    display: flex;
+    flex-direction: column;
+    -webkit-box-pack: center;
+    justify-content: center;
+    margin-left: 1rem;
+
+    .user-name {
+      font-size: 1.5rem;
+      line-height: 1.5;
+      font-weight: bold;
+      color: ${text1};
+
+      a {
+        color: inherit;
+        text-decoration: none;
+      }
+    }
+
+    .description {
+      white-space: pre-wrap;
+      font-size: 1.125rem;
+      line-height: 1.5;
+      margin-top: 0.25rem;
+      color: ${text2};
+      letter-spacing: -0.004em;
+    }
+  }
+`;
+
+const Border = styled.div`
+  width: 100%;
+  height: 1px;
+  margin-top: 2rem;
+  margin-bottom: 1.5rem;
+  background: ${backgroundElement3};
+`;
+
+export default User;
