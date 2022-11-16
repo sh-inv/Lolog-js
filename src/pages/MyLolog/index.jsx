5bc301d1f9a6784581edcb782423b98bd130d2e6
youbinpark <you8inpark@gmail.com>
2022-11-16 19:33:36 +0900

    Add: 내 블로그 네브바 레이아웃 및 기능 구현

--- src/pages/MyLolog/index.jsx
+++ src/pages/MyLolog/index.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import UserBox from '../../components/UserBox';
+import MyLogNav from '../../components/MyLogNavBar';
 import styled from 'styled-components';
 
 const MyLolog = () => {
   return (
     <MyLologContainer>
       <UserBox />
+      <MyLogNav />
       <Outlet />
     </MyLologContainer>
   );
