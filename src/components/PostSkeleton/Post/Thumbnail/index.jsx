3d38d1cc66654c4ef3dd8b3d1cfd17fac4af393c
sh.inv <qierapu1im@gmail.com>
2022-11-17 18:23:28 +0900

    Add: 글 섬네일 스켈레톤 컴포넌트 생성

---
+++ src/components/PostSkeleton/Post/Thumbnail/index.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Thumbnail = () => {
+  return <ThumbnailBox />;
+};
+
+const ThumbnailBox = styled.div`
+  height: 100%;
+  background-color: var(--bg-element4);
+`;
+
+export default Thumbnail;
