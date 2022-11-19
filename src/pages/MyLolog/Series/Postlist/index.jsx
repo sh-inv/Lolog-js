54d8cb24a0ee621e372c11cf4d54f7bc5a4c8339
youbinpark <you8inpark@gmail.com>
2022-11-20 06:27:44 +0900

    Add: 시리즈 포스트리스트 컴포넌트 생성

---
+++ src/pages/MyLolog/Series/Postlist/index.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const PostList = () => {
+  return (
+    <PostListContainer>
+      <label></label>
+    </PostListContainer>
+  );
+};
+
+const PostListContainer = styled.div``;
+
+export default PostList;
