35e8d878c3372d57a5809bc939f1e622fd717b33
youbinpark <you8inpark@gmail.com>
2022-11-23 04:54:16 +0900

    Add: 유저에 관한 정보 리덕스로 상태관리

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { themeModeReducer } from './modules/header';
 import { writeContentReducer } from './modules/write';
+import { userReducer } from './modules/user';
 
 const store = configureStore({
   reducer: {
     darkMode: themeModeReducer,
     writeContent: writeContentReducer,
+    user: userReducer,
   },
 });
 
--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,12 +1,36 @@
-import { configureStore, createSlice } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 
 const user = createSlice({
-  name: 'user',
-  initialState: 'Eden21',
-});
-
-export default configureStore({
-  reducer: {
-    user: user.reducer,
+  name: 'userReducer',
+  initialState: {
+    profileImage: null,
+    name: '',
+    intro: '',
+    lologTitle: '',
+    socialInfo: { email: '', github: '', twitter: '', facebook: '', url: '' },
+    email: '',
+  },
+  reducers: {
+    setProfileImage: (state, action) => {
+      state.profileImage = action.payload;
+    },
+    setName: (state, action) => {
+      state.name = action.payload;
+    },
+    setIntro: (state, action) => {
+      state.intro = action.payload;
+    },
+    setLologTitle: (state, action) => {
+      state.lologTitle = action.payload;
+    },
+    setSocialInfo: (state, action) => {
+      state.socialInfo = action.payload;
+    },
+    setEmail: (state, action) => {
+      state.email = action.payload;
+    },
   },
 });
+
+export const { setProfileImage, setName, setIntro, setLologTitle, setSocialInfo, setEmail } = user.actions;
+export const userReducer = user.reducer;
