4217b076710c1d30a9d251319a6a363b06f572ed
Goohyun3436 <rngus3436@g.kmou.ac.kr>
2022-11-22 15:02:43 +0900

    Fix: 새 글 작성 preview 화면 title, content 리덕스로 상태 관리

--- src/pages/Write/Preview/index.jsx
+++ src/pages/Write/Preview/index.jsx
@@ -1,13 +1,47 @@
+import { useSelector } from 'react-redux';
 import styled from 'styled-components';
 
 const Preview = () => {
-  return <PreviewContainer className='preview-container'></PreviewContainer>;
+  const { title, content } = useSelector(state => state.writeContent);
+
+  return (
+    <PreviewContainer className='preview-container'>
+      <h1 className='preview-title'>{title}</h1>
+      <div className='preview-content'>
+        <p>{content}</p>
+      </div>
+    </PreviewContainer>
+  );
 };
 
 const PreviewContainer = styled.div`
   padding: 3rem;
   background-color: var(--bg-element8);
   color: var(--text1);
+
+  h1 {
+    font-size: 2.5em;
+    margin-top: 1.675rem;
+    margin-bottom: 4rem;
+    font-weight: 800;
+  }
+
+  .preview-content {
+    overflow-wrap: break-word;
+    word-break: keep-all;
+    line-height: 1.7;
+    letter-spacing: -0.004em;
+    font-size: 1.125rem;
+    transition: color 0.125s ease-in 0s;
+
+    p {
+      display: block;
+      margin-block-start: 1em;
+      margin-block-end: 1em;
+      margin-inline-start: 0px;
+      margin-inline-end: 0px;
+    }
+  }
 `;
 
 export default Preview;
