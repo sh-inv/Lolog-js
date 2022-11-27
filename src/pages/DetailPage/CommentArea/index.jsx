edf8366c16c471aac148316db480ccf8c3212931
sh.inv <104422865+sh-inv@users.noreply.github.com>
2022-11-27 23:52:08 +0900

    Add: 댓글 입력 및 갯수 UI 구현 (#59)

--- src/pages/DetailPage/CommentArea/index.jsx
+++ src/pages/DetailPage/CommentArea/index.jsx
@@ -1,14 +1,51 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import Textarea from '../../../components/Comment/Textarea';
 import CommentList from './CommentList';
 
 const CommentArea = () => {
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const { data } = await axios.get('http://localhost:8000/inside/1/8');
+        setComments(data.comments);
+      } catch (error) {
+        console.log('comment list error => ', error);
+      }
+    })();
+    // (async () => {
+    //   try {
+    //     const { data } = await axios.get('/public/data/detailpage/comments.json');
+    //     setComments(data.comments);
+    //   } catch (error) {
+    //     console.log('comment list error => ', error);
+    //   }
+    // })();
+  }, []);
+  console.log(comments);
+
   return (
     <CommentAreaContainer>
-      <CommentList />
+      <h4 className='comments-count'>{comments.length}개의 댓글</h4>
+      <Textarea />
+      <CommentList comments={comments} />
     </CommentAreaContainer>
   );
 };
 
-const CommentAreaContainer = styled.div``;
+const CommentAreaContainer = styled.div`
+  margin-top: 3rem;
+  color: var(--text1);
+
+  .comments-count {
+    font-size: 1.125rem;
+    line-height: 1.5;
+    font-weight: 600;
+    margin-bottom: 1rem;
+  }
+`;
 
 export default CommentArea;
