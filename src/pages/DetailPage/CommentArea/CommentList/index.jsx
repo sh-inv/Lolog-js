04db075ed4a67360f2d1cebfe4b48e7ed4dc1cdb
sh.inv <qierapu1im@gmail.com>
2022-11-27 23:50:20 +0900

    Add: 댓글 입력 및 갯수 UI 구현

--- src/pages/DetailPage/CommentArea/CommentList/index.jsx
+++ src/pages/DetailPage/CommentArea/CommentList/index.jsx
@@ -3,28 +3,7 @@ import axios from 'axios';
 import styled from 'styled-components';
 import Comment from './Comment';
 
-const CommentList = () => {
-  const [comments, setComments] = useState([]);
-
-  useEffect(() => {
-    (async () => {
-      try {
-        const { data } = await axios.get('http://localhost:8000/inside/1/8');
-        setComments(data.comments);
-      } catch (error) {
-        console.log('comment list error => ', error);
-      }
-    })();
-    // (async () => {
-    //   try {
-    //     const { data } = await axios.get('/public/data/detailpage/comments.json');
-    //     setComments(data.comments);
-    //   } catch (error) {
-    //     console.log('comment list error => ', error);
-    //   }
-    // })();
-  }, []);
-
+const CommentList = ({ comments }) => {
   return (
     <CommentListContainer>
       {comments.map(commentData => (
