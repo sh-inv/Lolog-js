c11b0c997c236ef99a5eb452daf052641cd5a6fa
Goohyun3436 <rngus3436@g.kmou.ac.kr>
2022-11-28 12:21:10 +0900

    Refactor: 출간하기 API 통신 부분 bodyData 변수로 선언

--- src/pages/Write/UploadModal/ModalBtns/index.jsx
+++ src/pages/Write/UploadModal/ModalBtns/index.jsx
@@ -1,9 +1,9 @@
+import { useEffect } from 'react';
+import axios from 'axios';
 import { useDispatch, useSelector } from 'react-redux';
 import { setIsUploadModal } from '../../../../store/modules/write';
 import Button from '../../../../components/Button';
 import styled from 'styled-components';
-import axios from 'axios';
-import { useEffect } from 'react';
 
 const ModalBtns = () => {
   const { title, content, thumbnail, uploadType } = useSelector(state => state.writeContent);
@@ -12,12 +12,8 @@ const ModalBtns = () => {
   const onUpload = async () => {
     if (title && content) {
       try {
-        const response = await axios.put(`http://localhost:8000/posts?status=${uploadType}`, {
-          title: title,
-          content: content,
-          thumbnail: thumbnail,
-          tags: [],
-        });
+        const bodyData = { title: title, content: content, thumbnail: thumbnail, tags: [] };
+        const response = await axios.post(`http://localhost:8000/posts?status=${uploadType}`, bodyData);
         console.log(response);
       } catch (error) {
         console.log(error);
