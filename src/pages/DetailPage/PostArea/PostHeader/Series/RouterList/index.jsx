2c82ea6b93ce40a23b1a944c21b620af266f35ff
Goohyun3436 <rngus3436@g.kmou.ac.kr>
2022-12-08 03:09:21 +0900

    Refactor: 게시글 상세페이지 시리즈 목록 컴포넌트 css 순서 정리

--- src/pages/DetailPage/PostArea/PostHeader/Series/RouterList/index.jsx
+++ src/pages/DetailPage/PostArea/PostHeader/Series/RouterList/index.jsx
@@ -20,22 +20,24 @@ const RouterListContainer = styled.ol`
   padding-left: 0px;
   margin: 16px 0;
   line-height: 1.8;
-  font-size: 1rem;
   color: var(--text2);
+  font-size: 1rem;
   counter-reset: item 0;
+
   .current-post-link {
     color: var(--primary1);
     font-weight: bold;
   }
 
   li::before {
+    margin-right: 0.25rem;
     content: counter(item) '. ';
     counter-increment: item 1;
     color: var(--text3);
     font-style: italic;
-    margin-right: 0.25rem;
     pointer-events: none;
   }
+
   a:hover {
     text-decoration: underline;
   }
