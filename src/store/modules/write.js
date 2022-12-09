b8f78b507838ec207c752916585bffb1ecb70e3d
Goohyun3436 <rngus3436@g.kmou.ac.kr>
2022-12-09 17:24:23 +0900

    Refactor: 게시글 작성 페이지 언마운트시 리덕스 값 초기화

--- src/store/modules/write.js
+++ src/store/modules/write.js
@@ -1,25 +1,28 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+const initialState = {
+  title: '',
+  content: [],
+  thumbnail: '',
+  seriesId: null,
+  uploadType: '1',
+  uploadUrl: 'title',
+  discription: '',
+  isReverse: false,
+  isUploadModal: false,
+  isSeriesList: false,
+};
+
 const writeContentSlice = createSlice({
   name: 'writeContentReducer',
-  initialState: {
-    title: '',
-    content: [],
-    thumbnail: '',
-    seriesId: null,
-    uploadType: '1',
-    uploadUrl: 'title',
-    discription: '',
-    isReverse: false,
-    isUploadModal: false,
-    isSeriesList: false,
-  },
+  initialState: initialState,
   reducers: {
     setWriteContent: (state, action) => {
       state[action.payload.type] = action.payload.value;
     },
+    initialize: () => initialState,
   },
 });
 
-export const { setWriteContent } = writeContentSlice.actions;
+export const { setWriteContent, initialize } = writeContentSlice.actions;
 export const writeContentReducer = writeContentSlice.reducer;
