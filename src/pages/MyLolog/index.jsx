a908b9d751515f22b1fe7ccf09e9f273f4def14e
sh.inv <104422865+sh-inv@users.noreply.github.com>
2022-12-09 16:55:50 +0900

    Add: 내 벨로그 데이터 redux로 변경 (#85)

--- src/pages/MyLolog/index.jsx
+++ src/pages/MyLolog/index.jsx
@@ -5,15 +5,20 @@ import NavBar from './NavBar';
 import SearchBox from '../../components/SearchBox';
 import styled from 'styled-components';
 import { apiClient } from '../../api';
+import { useDispatch, useSelector } from 'react-redux';
+import { setMyLologData } from '../../store/modules/mainpostlist';
 
 const MyLolog = () => {
   const location = useLocation();
   const [userInfo, setUserInfo] = useState();
+  const dispatch = useDispatch();
+  // const { myLologData } = useSelector(state => state);
 
   useEffect(() => {
     (async () => {
       try {
         const { data } = await apiClient.get(`/lolog${location.pathname}?offset=1&limit=1&tag_id=`);
+        dispatch(setMyLologData(data));
         setUserInfo(data.user);
       } catch (error) {
         console.log('메인페이지 게시글 통신 오류 => ', error);
