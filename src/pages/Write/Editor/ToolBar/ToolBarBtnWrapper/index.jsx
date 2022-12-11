cc83ffa07c469832ab70286d78edf2e0a6d97720
Goohyun3436 <rngus3436@g.kmou.ac.kr>
2022-12-12 02:24:31 +0900

    Add: 에디터 프리뷰 code, a 태그 css 추가

--- src/pages/Write/Editor/ToolBar/ToolBarBtnWrapper/index.jsx
+++ src/pages/Write/Editor/ToolBar/ToolBarBtnWrapper/index.jsx
@@ -1,7 +1,12 @@
 import styled from 'styled-components';
+import { SlPicture } from 'react-icons/sl';
 
 const ToolBarBtnWrapper = ({ type }) => {
-  return <ToolBarBtnWrapperContainer className={type}></ToolBarBtnWrapperContainer>;
+  return (
+    <ToolBarBtnWrapperContainer className={type}>
+      <SlPicture />
+    </ToolBarBtnWrapperContainer>
+  );
 };
 
 const ToolBarBtnWrapperContainer = styled.button``;
--- src/pages/Write/Preview/index.jsx
+++ src/pages/Write/Preview/index.jsx
@@ -7,9 +7,7 @@ const Preview = () => {
   return (
     <PreviewContainer className='preview-container'>
       <h1 className='preview-title'>{title}</h1>
-      <div className='preview-content'>
-        <p>{content}</p>
-      </div>
+      <div dangerouslySetInnerHTML={{ __html: content }}></div>
     </PreviewContainer>
   );
 };
@@ -18,28 +16,28 @@ const PreviewContainer = styled.div`
   background-color: var(--bg-element8);
   color: var(--text1);
 
-  h1 {
+  .preview-title {
     font-size: 2.5em;
     margin-top: 1.675rem;
     margin-bottom: 4rem;
     font-weight: 800;
   }
 
-  .preview-content {
-    overflow-wrap: break-word;
-    word-break: keep-all;
-    line-height: 1.7;
-    letter-spacing: -0.004em;
-    font-size: 1.125rem;
-    transition: color 0.125s ease-in 0s;
+  img {
+    width: 100%;
+  }
 
-    p {
-      display: block;
-      margin-block-start: 1em;
-      margin-block-end: 1em;
-      margin-inline-start: 0px;
-      margin-inline-end: 0px;
-    }
+  code {
+    margin: 0;
+    background: var(--bg-element4);
+    color: var(--text1);
+    padding: 0.5em 1rem;
+    border-radius: 0.5em;
+  }
+
+  a {
+    color: blue;
+    cursor: pointer;
   }
 `;
 
