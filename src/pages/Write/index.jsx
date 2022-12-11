67b87354396d7691aff8fb751f2c693e2c07401b
Goohyun3436 <rngus3436@g.kmou.ac.kr>
2022-12-12 03:33:58 +0900

    Add: 게시글 작성 페이지에서 임시글 api 통신 기능 추가

--- src/pages/Write/index.jsx
+++ src/pages/Write/index.jsx
@@ -1,6 +1,11 @@
 import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import queryString from 'query-string';
 import { useSelector, useDispatch } from 'react-redux';
-import { initialize } from '../../store/modules/write';
+import { initialize, setWriteContent } from '../../store/modules/write';
+import { apiClient } from '../../api';
+import { toast } from 'react-toastify';
+import Toastify from '../../components/Toastify';
 import ReversePositionBtn from './ReversePositionBtn';
 import Editor from './Editor';
 import Preview from './Preview';
@@ -11,6 +16,33 @@ import { writeMaxWidth1024px, writeMaxWidth1920px, writeMaxWidth768px } from '..
 const Write = () => {
   const { isReverse, isUploadModal } = useSelector(state => state.writeContent);
   const dispatch = useDispatch();
+  const location = useLocation();
+
+  useEffect(() => {
+    const setInitialValue = async () => {
+      const savePost = queryString.parse(location.search);
+
+      if (savePost.id) {
+        try {
+          const config = {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem('authToken')}`,
+            },
+          };
+          const { post } = await apiClient.get(`/posts/saves/${savePost.id}`, config);
+          dispatch(setWriteContent({ type: 'title', value: post.title }));
+          dispatch(setWriteContent({ type: 'content', value: post.content }));
+          dispatch(setWriteContent({ type: 'thumbnail', value: post.thumbnail }));
+          dispatch(setWriteContent({ type: 'seriesId', value: post.seriesId }));
+          dispatch(setWriteContent({ type: 'description', value: post.description }));
+        } catch (error) {
+          toast.error('임시글 불러오기 실패');
+          console.log('write saves error =>', error);
+        }
+      }
+    };
+    setInitialValue();
+  }, [location.search]);
 
   useEffect(() => {
     return () => {
@@ -33,6 +65,7 @@ const Write = () => {
         </>
       )}
       {isUploadModal && <UploadModal />}
+      <Toastify />
     </WriteContainer>
   );
 };
