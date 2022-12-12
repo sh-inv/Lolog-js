48d609400fdc8abde98c1050c9f7dbd9cf92e07c
youbinpark <you8inpark@gmail.com>
2022-12-13 03:11:22 +0900

    Modify: 로그인 버튼 호버시 스타일 수정

--- src/layout/Header/index.jsx
+++ src/layout/Header/index.jsx
@@ -65,7 +65,7 @@ const Header = () => {
         </Content>
         {isToggleOpen && <ToggleMenuList toggleMenuRef={toggleMenuRef} setIsToggleOpen={setIsToggleOpen} />}
       </Positioner>
-      {isLoginModal ? <AuthModal isLoginModal={isLoginModal} setIsLoginModal={setIsLoginModal} /> : null}
+      {isLoginModal && <AuthModal isLoginModal={isLoginModal} setIsLoginModal={setIsLoginModal} />}
     </>
   );
 };
