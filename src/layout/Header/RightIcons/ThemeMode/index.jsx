c9f1f5109700847ded2f4cd5ca9a016a146d3fce
Goohyun3436 <rngus3436@g.kmou.ac.kr>
2022-12-15 03:44:27 +0900

    Fix: 게시글 작성 페이지 리버스 버튼 크기 확장

--- src/layout/Header/RightIcons/ThemeMode/index.jsx
+++ src/layout/Header/RightIcons/ThemeMode/index.jsx
@@ -19,7 +19,7 @@ const ThemeMode = () => {
   };
 
   return (
-    <ThemeModeContainer onClick={changeTheme}>
+    <ThemeModeContainer className='theme-mode-container' onClick={changeTheme}>
       <div className='setting-hover'>{isDarkMode ? <HiMoon /> : <BsFillSunFill />}</div>
     </ThemeModeContainer>
   );
