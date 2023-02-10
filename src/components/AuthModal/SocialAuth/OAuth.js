3594a73db015d47c39d7f8c8032fc195be99b84c
Eden Park <109200582+daydreamplace@users.noreply.github.com>
2023-02-10 20:51:36 +0900

    Delete OAuth.js

--- src/components/AuthModal/SocialAuth/OAuth.js
+++
@@ -1,3 +0,0 @@
-const GOOGLE_CLIENT_ID = '791472266211-1ccnb673c6ba9ckr5ut65h3crpuk4gda.apps.googleusercontent.com';
-const URL = 'http://localhost:5173/auth/google/callback';
-export const GOOGLE_URL = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${GOOGLE_CLIENT_ID}&redirect_uri=${URL}&scope=email profile&response_type=code`;
