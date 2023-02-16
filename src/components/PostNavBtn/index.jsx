e12ccb4b6f8a6c9b0a84b04082b8f8f731a98e08
sh.inv <104422865+sh-inv@users.noreply.github.com>
2023-02-16 17:36:58 +0900

    Fix: 상세페이지 이전 다음 포스트 css 깨지는 현상 수정 (#150)

--- src/components/PostNavBtn/index.jsx
+++ src/components/PostNavBtn/index.jsx
@@ -6,94 +6,81 @@ const PostNavBtn = ({ isLeft, postData }) => {
   const { post_id, title } = postData;
 
   return (
-    <PostNavBtnContainer isLeft={isLeft}>
-      <Link to={`/posts/${post_id}`} className='navigater'>
-        {isLeft && (
-          <div className='btn'>
-            <FiArrowLeft />
-          </div>
-        )}
-        <div className='content'>
-          <div className='post'>{isLeft ? '이전' : '다음'} 포스트</div>
-          <h3 className='text'>{title}</h3>
+    <PostNavBtnContainer to={`/posts/${post_id}`} className='navigater' isLeft={isLeft}>
+      {isLeft && (
+        <div className='btn'>
+          <FiArrowLeft className='arrow' />
         </div>
-        {!isLeft && (
-          <div className='btn right'>
-            <FiArrowLeft />
-          </div>
-        )}
-      </Link>
+      )}
+      <div className='content'>
+        <div className='post'>{isLeft ? '이전' : '다음'} 포스트</div>
+        <h3 className='text'>{title}</h3>
+      </div>
+      {!isLeft && (
+        <div className='btn'>
+          <FiArrowLeft className='arrow' />
+        </div>
+      )}
     </PostNavBtnContainer>
   );
 };
 
-const PostNavBtnContainer = styled.div`
+const PostNavBtnContainer = styled(Link)`
+  display: flex;
+  align-items: center;
+  justify-content: ${({ isLeft }) => !isLeft && 'flex-end'};
   width: 360px;
+  height: 4rem;
+  padding: 0 1rem;
+  background: var(--bg-element2);
 
-  .navigater {
+  .btn {
     display: flex;
+    justify-content: center;
     align-items: center;
-    width: 100%;
-    height: 4rem;
-    padding: 0 1rem;
-    box-shadow: rgb(0 0 0 / 6%) 0 0 4px 0;
-    background: var(--bg-element2);
-    cursor: pointer;
+    width: 32px;
+    height: 32px;
+    margin: ${({ isLeft }) => (isLeft ? '0 1rem 0 0' : '0 0 0 1rem')};
+    border: 1px solid var(--primary2);
+    border-radius: 16px;
 
-    .btn {
-<<<<<<< HEAD
-      width: 32px;
-      height: 32px;
-      border-radius: 16px;
-=======
-      min-width: 32px;
-      max-width: 32px;
-      height: 32px;
-      border-radius: 50%;
->>>>>>> develop
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      border: 1px solid var(--primary2);
+    .arrow {
       font-size: 1.5rem;
       color: var(--primary2);
-      margin: 0 1rem 0 0;
+      transform: ${({ isLeft }) => !isLeft && 'rotate(180deg)'};
+    }
+  }
+
+  .content {
+    ${({ isLeft }) => !isLeft && `display: flex; flex-direction: column; align-items: flex-end;`}
+    width: calc(100% - 32px - 1rem);
+    line-height: 1;
+    color: var(--text2);
+
+    .post {
+      font-size: 0.75rem;
+      font-weight: bold;
     }
 
-    .right {
-      margin: 0 0 0 1rem;
-      transform: rotate(180deg);
-    }
-
-    .content {
-      flex: 1 1 0%;
-      line-height: 1;
-      color: var(--text2);
-
-      .post {
-        text-align: ${({ isLeft }) => (isLeft ? 'left' : 'right')};
-        font-size: 0.75rem;
-      }
-
-      .text {
-        width: 100%;
-        font-size: 1.125rem;
-        line-height: 1.15;
-        margin-top: 0.5rem;
-        text-overflow: ellipsis;
-        white-space: nowrap;
-        overflow: hidden;
-        text-align: ${({ isLeft }) => (isLeft ? '' : 'right')};
-      }
+    .text {
+      width: 100%;
+      font-size: 1.125rem;
+      text-align: ${({ isLeft }) => !isLeft && 'right'};
+      margin-top: 0.5rem;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap;
     }
   }
 
   @media screen and (max-width: 768px) {
     width: 100%;
 
-    .navigater {
-      margin: ${({ isLeft }) => (isLeft ? '0 0 1.5rem 0' : '0')};
+    :nth-last-child(2) {
+      margin-bottom: 1.5rem;
+    }
 
+    .content {
       .text {
         font-size: 1rem;
       }
